9ff7a674e0cdb499d8b4ca77f367c9eb75aa88bd
BAGPIpes <bgreenb4@terpmail.umd.edu>
2025-10-12 22:25:42 +0100

    Lab 6 finished

--- bug.py
+++ bug.py
@@ -4,7 +4,11 @@ from shifter import ShifterClass
 from Bug import BugClass
 
 serialPin, latchPin, clockPin = 23, 24, 25
-onCatch, wrapCatch, speedCatch = 16, 20, 21
+onCatch, speedCatch, wrapCatch = 16, 20, 21
+onState, wrapState, speedState = False, False, False
+
+shift = ShifterClass(serialPin, clockPin, latchPin)
+bugout = BugClass(shift)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(serialPin, GPIO.OUT)
@@ -16,10 +20,48 @@ GPIO.setup(wrapCatch, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup(speedCatch, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 def onCallback(pin):
-	global bugout
+	global onState
+	onState = not onState
+	print(f"on/off toggled to {onState}")
+
+def wrapCallback(pin):
+	global wrapState
+	wrapState = not wrapState
+	print(f"wrap toggled to {wrapState}")
+
+def speedUpCallback(pin):
+	global speedState
+	speedState = not speedState
+	print(f"speed toggled to {speedState}")
+
+def run():
+	global bugout, wrapOn, speed, wrapState, speedState, onState, shift
+	
+	if wrapState == True:
+		wrapOn = True
+	else:
+		wrapOn = False
+	if speedState == True:
+		speed = 0.03
+	else:
+		speed = 0.1
+		
+	bugout.timestep = speed
+	bugout.isWrapOn = wrapOn 
+	
+	if onState == True:
+		bugout.start()
+	else:
+		bugout.stop()
+
+GPIO.add_event_detect(onCatch, GPIO.BOTH, callback = onCallback, bouncetime = 100)
+GPIO.add_event_detect(wrapCatch, GPIO.BOTH, callback = wrapCallback, bouncetime = 100)
+GPIO.add_event_detect(speedCatch, GPIO.BOTH, callback = speedUpCallback, bouncetime = 100)
+
+try:
+	while 1:
+		run()
+except KeyboardInterrupt:
+	print("Exiting")
 	bugout.stop()
-
-GPIO.add_event_detect(onCatch, GPIO.RISING, callback = onCallback, bouncetime = 100)
-
-shift = ShifterClass(serialPin, clockPin, latchPin)
-bugout = BugClass(shift)
+	GPIO.cleanup()
